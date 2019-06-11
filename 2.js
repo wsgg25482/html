dfae5e6a7af3738a0f89e82f3c982c25ef5798de
wsgg25482 <39294954+wsgg25482@users.noreply.github.com>
2019-06-11 11:15:46 +0800

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,24 @@
+window.onload = function(){
+    $("h1.a").hide();
+	$("h2.a").hide();
+}
+
+function Showh1() {
+    $("h1.a").slideDown(1000);
+}
+
+function Showh2() {
+    $("h2.a").slideDown(1000);
+}
+
+function Showh3() {
+    $("h3.a").slideDown(1000);
+}
+
+function Showh4() {
+    $("h4.a").slideDown(1000);
+}
+
+function Showh5() {
+    $("h5.a").slideDown(1000);
+}
